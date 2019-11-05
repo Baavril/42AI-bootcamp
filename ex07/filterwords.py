5abb23ba37b3e62dbfa10d31a24580261e74460f
Baptiste AVRIL <baavril@e1r1p9.42.fr>
2019-11-05 16:46:33 +0100

    feat[ex07] words filter

---
+++ ex07/filterwords.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python3.7
+
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    filterwords.py                                     :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: baavril <baavril@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2019/11/05 15:37:02 by baavril           #+#    #+#              #
+#    Updated: 2019/11/05 15:37:02 by baavril          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import sys
+import re
+
+if __name__ == '__main__':
+
+    nb = int(sys.argv[2])
+    lw = re.sub('\b[a-zA-Z0-9]\s', '', sys.argv[1])
+    lw = re.split(' +', lw)
+    print([w for w in lw if len(w) > int(sys.argv[2])])
