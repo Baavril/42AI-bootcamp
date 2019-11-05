e2be5942b90c9e757e9e3c68ad3664e77d7e23a9
Baptiste AVRIL <baavril@e1r1p9.42.fr>
2019-11-05 08:11:03 +0100

    feat(kata01)

--- ex05/kata01.py
+++ ex05/kata01.py
@@ -12,11 +12,13 @@
 #                                                                              #
 # **************************************************************************** #
 
-languages = {
-'Python': 'Guido van Rossum',
-'Ruby': 'Yukihiro Matsumoto',
-'PHP': 'Rasmus Lerdorf',
-}
 
-if __name__ == '__main__'
+if __name__ == '__main__':
 
+    languages = {
+            'Python': 'Guido van Rossum',
+            'Ruby': 'Yukihiro Matsumoto',
+            'PHP': 'Rasmus Lerdorf',
+            }
+    for key in languages:
+        print(key + " was created by " + languages[key])
